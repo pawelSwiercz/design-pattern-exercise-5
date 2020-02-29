025a30aca62da61adfa08ae3815b553b526d1c90
pawelSwiercz <pawel.swiercz@yahoo.pl>
2020-02-29 15:07:22 +0100

    AuctionService Singleton

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -12,10 +12,10 @@ public class Main {
         Auction auction = new Auction(1,"Coffee",null,
                 LocalDateTime.now().plusDays(2L),seller,0,null);
 
-        AuctionService auctionService = new AuctionService();
-        auctionService.bid(auction,new Customer("Anna Kowalik"),20);
 
-        auctionService.bid(auction,new Customer("Krzysztof Adamowicz"),25);
+        AuctionService.getInstance().bid(auction,new Customer("Anna Kowalik"),20);
+
+        AuctionService.getInstance().bid(auction,new Customer("Krzysztof Adamowicz"),25);
 
 
         System.out.println("Auction with id: " + auction.getId()+ " finished. Highest bid: " + auction.getHighestBid() + " PLN.");
--- src/main/java/model/AuctionService.java
+++ src/main/java/model/AuctionService.java
@@ -1,8 +1,25 @@
 package model;
 
 public class AuctionService {
+    private static AuctionService instance;
+
+    private AuctionService() {
+    }
+
+    public static AuctionService getInstance() {
+        if (instance == null) {
+            synchronized (AuctionService.class) {
+                if (instance == null) {
+                    instance = new AuctionService();
+                }
+            }
+        }
+        return instance;
+    }
+
+
     public boolean bid(Auction auction, Customer buyer, double amount) {
-        if (auction.getHighestBid()<amount) {
+        if (auction.getHighestBid() < amount) {
             auction.setHighestBid(amount);
             auction.setBuyer(buyer);
             return true;
